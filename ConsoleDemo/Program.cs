9c2e9a44c91afddb5da45d4b974627e0ffcf8d2a
Loug522 <lucianoguasque@gmail.com>
2025-06-15 12:19:15 -0300

    nm today

--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            System.Console.Write("Please type your name: ");
+            Console.Write("Please type your name: ");
             var name = Console.ReadLine();
-            Console.WriteLine($"Hello, {name}");            
+            Console.WriteLine($"Hello, {name}");
         }
     }
 }
